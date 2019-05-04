6c7c46d6bc5e39e9856050c7169c6d92d948afec
Lazhari <lazhari.mohammed@outlook.com>
2019-05-04 23:17:16 +0100

    Implement the links list

---
+++ src/components/LinkList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "./Link";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        url
+        description
+        createdAt
+      }
+    }
+  }
+`;
+
+const LinkList = props => {
+  return (
+    <div>
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching..</div>;
+          if (error) return <div>Error</div>;
+          const linksToRender = data.feed.links;
+          return (
+            <div>
+              {linksToRender.map(link => (
+                <Link key={link.id} link={link} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
+    </div>
+  );
+};
+
+export default LinkList;
