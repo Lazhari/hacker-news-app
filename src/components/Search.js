79b96ac1993e0868a82e30559ed665fc651ff5f1
Lazhari <lazhari.mohammed@outlook.com>
2019-05-10 21:47:28 +0000

    Redesigned the search page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import { withApollo } from "react-apollo";
+import { createStyles, withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
 import gql from "graphql-tag";
 import Link from "./Link";
 
@@ -26,7 +31,7 @@ const FEED_SEARCH_QUERY = gql`
   }
 `;
 
-const Search = ({ client }) => {
+const Search = ({ client, classes }) => {
   const [state, setState] = useState({
     links: [],
     filter: ""
@@ -47,21 +52,47 @@ const Search = ({ client }) => {
 
   return (
     <div>
-      <div>
-        Search
-        <input
-          type="text"
-          onChange={e => setState({ ...state, filter: e.target.value })}
-        />
-        <button onClick={() => _executeSearch()}>
-          {loading ? "Loading..." : "Search"}
-        </button>
+      <div className={classes.headerContainer}>
+        <Paper className={classes.searchBox}>
+          <TextField
+            className={classes.searchInput}
+            fullWidth
+            label="Search"
+            variant="outlined"
+            type="text"
+            onChange={e => setState({ ...state, filter: e.target.value })}
+          />
+          <Button onClick={() => _executeSearch()} size="large">
+            {loading ? "Loading..." : "Search"}
+          </Button>
+        </Paper>
       </div>
-      {state.links.map((link, index) => (
-        <Link key={link.id} link={link} index={index} />
-      ))}
+      <Table>
+        {state.links.map((link, index) => (
+          <Link key={link.id} link={link} index={index} />
+        ))}
+      </Table>
     </div>
   );
 };
 
-export default withApollo(Search);
+const styles = ({ spacing }) =>
+  createStyles({
+    headerContainer: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    searchBox: {
+      width: "80%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: spacing.unit * 2
+    },
+    searchInput: {
+      marginRight: spacing.unit * 2
+    }
+  });
+
+export default withStyles(styles)(withApollo(Search));
